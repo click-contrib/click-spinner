090b3491a61e4076c9ac0b115247d6896c1f00a6
Yoav Ram <github@yoavram.com>
2016-12-15 10:57:57 +0200

    beep when done; style changes

--- click_spinner/__init__.py
+++ click_spinner/__init__.py
@@ -7,13 +7,14 @@ import itertools
 class Spinner(object):
     spinner_cycle = itertools.cycle(['-', '/', '|', '\\'])
 
-    def __init__(self, force=False):
-        self._force = force
+    def __init__(self, beep=False, force=False):
+        self.beep = beep
+        self.force = force
         self.stop_running = None
         self.spin_thread = None
 
     def start(self):
-        if sys.stdout.isatty() or self._force:
+        if sys.stdout.isatty() or self.force:
             self.stop_running = threading.Event()
             self.spin_thread = threading.Thread(target=self.init_spin)
             self.spin_thread.start()
@@ -36,28 +37,34 @@ class Spinner(object):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
+        if self.beep:
+            print("\7", end='', flush=True)
         return False
 
 
-def spinner(force=False):
+def spinner(beep=False, force=False):
     """This function creates a context manager that is used to display a
     spinner on stdout as long as the context has not exited.
 
     The spinner is created only if stdout is not redirected, or if the spinner
     is forced using the `force` parameter.
 
-    Parameters:
+    Parameters
+    ----------
+    beep : bool
+        Beep when spinner finishes.
+    force : bool
+        Force creation of spinner even when stdout is redirected.
 
-      force (bool): Force creation of spinner even when stdout is redirected.
-
-    Example usage::
+    Example
+    -------
 
         with spinner():
             do_something()
             do_something_else()
 
     """
-    return Spinner(force)
+    return Spinner(beep, force)
 
 
 from ._version import get_versions

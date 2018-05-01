a604a1eb6811b305d50ab112a523a7b16e346856
Adrien Pensart <adrien.pensart@corp.ovh.com>
2018-05-01 09:29:44 -0400

    test added

--- tests/test_spinner.py
+++ tests/test_spinner.py
@@ -79,6 +79,26 @@ def test_spinner_redirect_force():
     assert result.exception is None
 
 
+def test_spinner_disable():
+    @click.command()
+    def cli():
+       stdout_io = StringIO()
+       saved_stdout = sys.stdout
+       sys.stdout = stdout_io  # redirect stdout to a string buffer
+       spinner = click_spinner.Spinner(disable=True)
+       spinner.start()
+       time.sleep(1)  # allow time for doing nothing
+       spinner.stop()
+       sys.stdout = saved_stdout
+       stdout_io.flush()
+       stdout_str = stdout_io.getvalue()
+       assert len(stdout_str) == 0
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [])
+    assert result.exception is None
+
+
 def test_spinner_as():
     @click.command()
     def cli():

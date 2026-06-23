c95945d4029dc0e2b14fa1b046a1033e4d6fe9fa
Baptiste Fontaine <b@ptistefontaine.fr>
2026-06-23 21:22:12 +0200

    Add type hints

--- ci.sh
+++ ci.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -ex
 
 uv run ruff check click_spinner
-#uv run mypy --check-untyped-defs --explicit-package-bases --strict click_spinner
-uv run mypy click_spinner
+uv run mypy --check-untyped-defs --strict click_spinner
 uv run pytest tests
--- click_spinner/__init__.py
+++ click_spinner/__init__.py
@@ -1,20 +1,22 @@
+import itertools
 import sys
 import threading
-import itertools
+from typing import Any, TextIO, Union
 
 
 class Spinner(object):
     spinner_cycle = itertools.cycle(['-', '/', '|', '\\'])
 
-    def __init__(self, beep=False, disable=False, force=False, stream=sys.stdout):
-        self.disable = disable
-        self.beep = beep
-        self.force = force
-        self.stream = stream
-        self.stop_running = None
-        self.spin_thread = None
+    def __init__(self, beep: bool = False, disable: bool = False, force: bool = False,
+                 stream: TextIO = sys.stdout) -> None:
+        self.disable: bool = disable
+        self.beep: bool = beep
+        self.force: bool = force
+        self.stream: TextIO = stream
+        self.stop_running: Union[threading.Event, None] = None
+        self.spin_thread: Union[threading.Thread, None] = None
 
-    def start(self):
+    def start(self) -> None:
         if self.disable:
             return
         if self.stream.isatty() or self.force:
@@ -22,27 +24,29 @@ class Spinner(object):
             self.spin_thread = threading.Thread(target=self.init_spin)
             self.spin_thread.start()
 
-    def stop(self):
+    def stop(self) -> None:
         if self.spin_thread:
-            self.stop_running.set()
+            if self.stop_running:
+                self.stop_running.set()
             self.spin_thread.join()
 
-    def init_spin(self):
-        while not self.stop_running.is_set():
-            self.stream.write(next(self.spinner_cycle))
-            self.stream.flush()
-            self.stop_running.wait(0.25)
-            self.stream.write('\b')
+    def init_spin(self) -> None:
+        if self.stop_running:
+            while not self.stop_running.is_set():
+                self.stream.write(next(self.spinner_cycle))
+                self.stream.flush()
+                self.stop_running.wait(0.25)
+                self.stream.write('\b')
 
         self.stream.write(' ')
         self.stream.write('\b')
         self.stream.flush()
 
-    def __enter__(self):
+    def __enter__(self) -> "Spinner":
         self.start()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
         if self.disable:
             return False
         self.stop()
@@ -52,7 +56,7 @@ class Spinner(object):
         return False
 
 
-def spinner(beep=False, disable=False, force=False, stream=sys.stdout):
+def spinner(beep: bool = False, disable: bool = False, force: bool = False, stream: TextIO = sys.stdout) -> Spinner:
     """This function creates a context manager that is used to display a
     spinner on stdout as long as the context has not exited.
 

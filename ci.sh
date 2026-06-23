59cd67db7b78c23869935575ae87afb6d8be2bae
Baptiste Fontaine <b@ptistefontaine.fr>
2026-06-23 21:07:02 +0200

    CI: check with mypy

--- ci.sh
+++ ci.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -ex
 
-#uv run ruff check click_spinner
-#uv run mypy --strict --check-untyped-defs --explicit-package-bases --strict click_spinner
+uv run ruff check click_spinner
+#uv run mypy --check-untyped-defs --explicit-package-bases --strict click_spinner
+uv run mypy click_spinner
 uv run pytest .
